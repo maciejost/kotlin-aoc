1fffed028514501ab4e731487338f9ae4612d1ba
Maciej Ostrowski <maciej.ostrowski@fremtind.no>
2024-08-04 15:48:57 +0200

    Finish day 1

---
+++ src/main/kotlin/DayOne.kt
@@ -0,0 +1,79 @@
+import java.io.File
+import java.io.BufferedReader
+
+
+private fun parseElfData(): List<Int> {
+    val bufferedReader: BufferedReader = File("inputs/day1.txt").bufferedReader()
+    val inputString = bufferedReader.use { it.readText() }
+
+    val elves = inputString.split("\n\n").map { it.split("\n") }
+    val washedValueElves = elves.map { it ->
+        it.map {
+            if (it == "" || it == "\n") {
+                0
+            } else {
+                it.toInt()
+            }
+        }
+    }
+
+
+
+    return washedValueElves.map { it.sum() }
+}
+
+interface Elf {
+    val calories: Int
+    val index: Int
+}
+
+private fun getHighestCalories(): Elf {
+    val elves = parseElfData()
+    var highestElf = elves.maxOrNull()
+
+    var elfObject = object : Elf {
+        override val calories: Int
+            get() = highestElf!!
+        override val index: Int
+            get() = elves.indexOf(highestElf)
+    }
+
+    return elfObject;
+}
+
+private fun getTopThree(): List<Elf> {
+    val elves = parseElfData()
+    val sortedElves = elves.sortedDescending()
+    val topThree = sortedElves.take(3)
+    val topThreeElves = topThree.map {
+        var elfObject = object : Elf {
+            override val calories: Int
+                get() = it
+            override val index: Int
+                get() = elves.indexOf(it)
+        }
+        elfObject
+    }
+
+    return topThreeElves
+}
+
+fun dayOne() {
+    println("Day One \n")
+    val highestElf = getHighestCalories()
+    val topThreeElves = getTopThree()
+
+    val topThreeSum = topThreeElves.sumOf { it.calories }
+
+    println("Top three elves:")
+    topThreeElves.forEach {
+        println("${it.index}, ${it.calories}")
+    }
+    println()
+
+    println("Top elf: ${highestElf.index}, ${highestElf.calories}\n")
+
+    println("Sum of top three: $topThreeSum")
+}
+
+
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    dayOne()
+    /*println("Enter puzzle day:")
+    val puzzleDay = readln()
+
+    if (puzzleDay == "1") {
+    }*/
+}
